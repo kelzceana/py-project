88ce5bce13b3e4714873522ec190fa241d9b96e6
kelechiobikaonu <kelechipauline@gmail.com>
2025-01-31 11:59:12 -0500

    refactored code to a function

--- automation-scripts/ec2_status_check.py
+++ automation-scripts/ec2_status_check.py
@@ -4,11 +4,14 @@ ec2_resource = boto3.resource('ec2')
 
 ec2_statuses = ec2_client.describe_instance_status()
 
-for ec2_status in ec2_statuses['InstanceStatuses']:
-    instance_id = ec2_status['InstanceId']
-    instance_status = ec2_status['InstanceStatus']['Status']
-    system_status = ec2_status.get('SystemStatus').get('Status')
+def status_check():
+    for ec2_status in ec2_statuses['InstanceStatuses']:
+        instance_id = ec2_status['InstanceId']
+        instance_status = ec2_status['InstanceStatus']['Status']
+        system_status = ec2_status.get('SystemStatus').get('Status')
 
-    print(f'Instance Status Check of instance {instance_id} is {instance_status}')
-    print(f'System Status Check of instance {instance_id} is {system_status}')
+        print(f'Instance Status Check of instance {instance_id} is {instance_status}')
+        print(f'System Status Check of instance {instance_id} is {system_status}')
 
+
+status_check()
